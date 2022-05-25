6b6799e80bef6023b248ff57858f77d7b1a69ac0
Daniele Marchetta <danymar20.9@gmail.com>
2022-05-25 15:45:49 +0200

    Aggiunta items.map

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ export const funzione = (s,type) => {
   const url = BASE_URL+"s="+s+"&type="+type;
   fetch(url)
     .then((response) => response.json())
-    .then((prova) => console.table(prova));
+    .then((prova) => {
+      //console.table(prova)
+      const oggetto = prova.Search;
+      viewItems(oggetto);}
+      );
+};
+
+const viewItems = (items) => {
+  items.map((item) => {
+
+    console.group(item.Title);
+    console.log(item.Year);
+    console.log(item.Poster);
+    console.groupEnd();
+
+  });
 };
 
