8ac9eafe6e880f5e46ffce9b0245db2885312081
Daniele Marchetta <danymar20.9@gmail.com>
2022-05-25 11:55:23 +0200

    seconda_fetch

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const url = "https://www.omdbapi.com/?apikey=fa602d3f&t=terminator&y=2012";
 
+const url2= "https://www.omdbapi.com/?apikey=fa602d3f&s=terminator"
+
+const url3= "https://www.omdbapi.com/?apikey=fa602d3f&s=terminator&type=series"
+
 export const funzione = () => {
   fetch(url)
     .then((response) => response.json())
-    .then((prova) => console.log(prova.Released));
+    .then((prova) => console.table(prova));
 };
+
+export const funzione2 = () => {
+    fetch(url2)
+      .then((response) => response.json())
+      .then((prova) => {
+          const movies = prova.Search;
+          console.table(movies)}
+          );
+  };
+
+  export const funzione3 = () => {
+    fetch(url3)
+      .then((response) => response.json())
+      .then((prova) => {
+          const movies = prova.Search;
+          console.table(movies)}
+          );
+  };
