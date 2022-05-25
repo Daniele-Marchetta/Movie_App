061f7b0f29a12018fd10174aa068934f397e3161
Daniele Marchetta <danymar20.9@gmail.com>
2022-05-25 14:45:03 +0200

    fetch v3.0

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-const url = "https://www.omdbapi.com/?apikey=fa602d3f&t=terminator&y=2012";
+import {BASE_URL} from "./config.js";
 
-const url2= "https://www.omdbapi.com/?apikey=fa602d3f&s=terminator"
 
-const url3= "https://www.omdbapi.com/?apikey=fa602d3f&s=terminator&type=series"
-
-export const funzione = () => {
+export const funzione = (s,type) => {
+  const url = BASE_URL+"s="+s+"&type="+type;
   fetch(url)
     .then((response) => response.json())
     .then((prova) => console.table(prova));
 };
 
-export const funzione2 = () => {
-    fetch(url2)
-      .then((response) => response.json())
-      .then((prova) => {
-          const movies = prova.Search;
-          console.table(movies)}
-          );
-  };
-
-  export const funzione3 = () => {
-    fetch(url3)
-      .then((response) => response.json())
-      .then((prova) => {
-          const movies = prova.Search;
-          console.table(movies)}
-          );
-  };
